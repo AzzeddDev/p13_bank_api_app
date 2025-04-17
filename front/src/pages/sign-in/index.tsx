43f1a846ffa55add66b211b89cb5c3fe8234fff6
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-04-17 08:41:08 +0200

    add sign-in components with hooks and set token when sign-in

--- front/src/pages/sign-in/index.tsx
+++ front/src/pages/sign-in/index.tsx
@@ -1,5 +1,67 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import {loginUser} from "../../api/auth"
+
 export function SignIn() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setError("")
+
+        try {
+            const data = await loginUser(email, password)
+            // token
+            localStorage.setItem("token", data.body.token)
+            navigate("/dashboard")
+
+        } catch (err: any) {
+            console.error("Login Error:", err.response?.data || err)
+            setError("Invalid email or password")
+        }
+    }
+
     return (
-        <></>
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+                <i className="fa fa-user-circle sign-in-icon"></i>
+                <h1>Sign In</h1>
+
+                {error && <p className="error-message">{error}</p>}
+
+                <form onSubmit={handleSubmit}>
+                    <div className="input-wrapper">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-remember">
+                        <input type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember me</label>
+                    </div>
+                    <button type="submit" className="sign-in-button">
+                        Sign In
+                    </button>
+                </form>
+            </section>
+        </main>
     )
 }
