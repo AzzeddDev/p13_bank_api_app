5339a849884c91541b1a122602112411b5efae0f
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-08-06 08:31:48 +0200

    add ts@ignore for code error

--- front/src/hooks/useUserProfile.ts
+++ front/src/hooks/useUserProfile.ts
@@ -22,6 +22,7 @@ export function useUserProfile() {
                 setUserProfile(profile.body)
                 setLoading(false)
             } catch (error) {
+                // @ts-ignore
                 setError(error.message || "Error fetching profile data")
                 setLoading(false)
             }
@@ -40,6 +41,7 @@ export function useUserProfile() {
             const updatedProfile = await updateUserProfile(token, firstName, lastName)
             setUserProfile(updatedProfile.body)
         } catch (error) {
+            // @ts-ignore
             setError(error.message || "Error updating profile")
         }
     }
