fb6923da9cf5a52b0e3ee205e71ac7c936e71e9f
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-09-14 23:08:31 +0200

    add account section transaction as component for futur update :)

--- front/src/pages/user/index.tsx
+++ front/src/pages/user/index.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { ChangeNameModal } from "../../component/modal/ChangeNameModal"
 import {RootState} from "../../redux/store";
 import {fetchUserProfile, updateUserName} from "../../redux/store/userSlice";
+import AccountSection from "../../component/account-section";
 
 export function Dashboard() {
     const dispatch = useDispatch()
@@ -44,38 +45,29 @@ export function Dashboard() {
             </div>
 
             <h2 className="sr-only">Accounts</h2>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                    <p className="account-amount">$2,082.79</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
 
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                    <p className="account-amount">$10,928.42</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
+            {/* add in futur loop here */}
+            <AccountSection
+                title="Argent Bank Checking (x8349)"
+                amount="$2,082.79"
+                description="Available Balance"
+                onViewTransactions={() => console.log("Checking transactions")}
+            />
 
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                    <p className="account-amount">$184.30</p>
-                    <p className="account-amount-description">Current Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
+            <AccountSection
+                title="Argent Bank Savings (x6712)"
+                amount="$10,928.42"
+                description="Available Balance"
+                onViewTransactions={() => console.log("Checking transactions")}
+            />
+
+            <AccountSection
+                title="Argent Bank Credit Card (x8349)"
+                amount="$184.30"
+                description="Current Balance"
+                onViewTransactions={() => console.log("Checking transactions")}
+            />
+
         </main>
     )
 }
