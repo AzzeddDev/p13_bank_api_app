7d1377d7d21a395274c18e10d2a5113fd4ff3b99
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-09-14 23:01:35 +0200

    add missing html code from template

--- front/src/pages/user/index.tsx
+++ front/src/pages/user/index.tsx
@@ -23,22 +23,59 @@ export function Dashboard() {
     if (loading) return <div><h1>Chargements !...</h1></div>
     if (error) return <div><h1>Erreur: {error}</h1></div>
 
+    // @ts-ignore
     return (
-        <main className="main bg-dark pad-4">
+        <main className="mainUser bg-dark pad-4">
             <div className="header">
                 <h1>
-                    Welcome back<br />
+                    Welcome back<br/>
                     {profile?.firstName} {profile?.lastName}!
                 </h1>
-                <button onClick={() => setIsModalOpen(true)}>Change Name</button>
+                <button className={"edit-button"} onClick={() => setIsModalOpen(true)}>Edit Name</button>
 
-                {isModalOpen && (
-                    <ChangeNameModal
-                        onClose={() => setIsModalOpen(false)}
-                        onSave={handleNameChange}
-                    />
-                )}
+                <div className={"modalBody"}>
+                    {isModalOpen && (
+                        <ChangeNameModal
+                            onClose={() => setIsModalOpen(false)}
+                            onSave={handleNameChange}
+                        />
+                    )}
+                </div>
             </div>
+
+            <h2 className="sr-only">Accounts</h2>
+            <section className="account">
+                <div className="account-content-wrapper">
+                    <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+                    <p className="account-amount">$2,082.79</p>
+                    <p className="account-amount-description">Available Balance</p>
+                </div>
+                <div className="account-content-wrapper cta">
+                    <button className="transaction-button">View transactions</button>
+                </div>
+            </section>
+
+            <section className="account">
+                <div className="account-content-wrapper">
+                    <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+                    <p className="account-amount">$10,928.42</p>
+                    <p className="account-amount-description">Available Balance</p>
+                </div>
+                <div className="account-content-wrapper cta">
+                    <button className="transaction-button">View transactions</button>
+                </div>
+            </section>
+
+            <section className="account">
+                <div className="account-content-wrapper">
+                    <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+                    <p className="account-amount">$184.30</p>
+                    <p className="account-amount-description">Current Balance</p>
+                </div>
+                <div className="account-content-wrapper cta">
+                    <button className="transaction-button">View transactions</button>
+                </div>
+            </section>
         </main>
     )
 }
